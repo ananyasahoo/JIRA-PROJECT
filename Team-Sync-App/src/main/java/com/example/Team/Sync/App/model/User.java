db63c2903110a6acb2d09ac00427291ec8108e07
ananyasahoo <priyadarshiniananya776@gmail.com>
2024-08-11 17:46:00 +0530

    task and project creation added

---
+++ Team-Sync-App/src/main/java/com/example/Team/Sync/App/model/User.java
@@ -0,0 +1,28 @@
+package com.example.Team.Sync.App.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Long id ;
+    private String  user_name ;
+    private String email ;
+    private String phone;
+    private String password;
+    private Role role;
+    private Long department_id ;
+
+    public enum Role {
+        SCRUM_MASTER,
+        DEVELOPER,
+        NORMAL_USER,
+        TESTER,
+        MANAGER
+    }
+
+}
